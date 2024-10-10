f23fd0e89da3e9689905b51023d2a9e520f399cd
maimouna <maymounaijdag@gmail.com>
2024-10-10 11:32:18 +0200

    add birthday countdown

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, request
 from datetime import datetime
+from datetime import datetime, date
+
+def days_to_birthday(dob):
+    today = date.today()
+    dob = datetime.strptime(dob, "%Y-%m-%d").date()
+    next_birthday = date(today.year, dob.month, dob.day)
+    if next_birthday < today:
+        next_birthday = date(today.year + 1, dob.month, dob.day)
+    return (next_birthday - today).days
+
 
 def calculate_age(dob):
     today = datetime.today()
@@ -27,7 +37,8 @@ def index():
         dob = request.form['dob']
         age = calculate_age(dob)
         zodiac = get_zodiac_sign(dob)
-        message = f"Welcome, {name}! You are {age} years old. Your zodiac sign is {zodiac}."
+        days_to_bday = days_to_birthday(dob)
+        message = f"Welcome, {name}! You are {age} years old. Your zodiac sign is {zodiac}. There are {days_to_bday} days until your next birthday!"
         return render_template('result.html', message=message)
     return render_template('index.html')
 
